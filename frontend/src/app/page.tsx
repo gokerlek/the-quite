883d14c18b8dc6e9c53fbfe90bf7c2ab2d4b52eb
Göker ZAFER <gokerlek@gmail.com>
2025-09-07 01:30:59 +0300

    Update pointer-events style for section to improve interaction handling

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -139,7 +139,7 @@ export default function Home() {
 
         <section
           id='the_quite'
-          className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 opacity-0'
+          className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 opacity-0 pointer-events-none'
         >
           <svg
             width='480'
