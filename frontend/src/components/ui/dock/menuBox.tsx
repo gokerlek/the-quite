0c0faacfc04620fc560e7e3ce10f100b02714d70
Göker ZAFER <gokerlek@gmail.com>
2025-10-21 19:24:39 +0300

    Fix menuBox styling; remove unnecessary negative z-index for improved layout

--- frontend/src/components/ui/dock/menuBox.tsx
+++ frontend/src/components/ui/dock/menuBox.tsx
@@ -9,7 +9,7 @@ export const MenuBox = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivEl
     return (
       <div
         ref={ref}
-        className='h-12 px-2 flex items-center justify-center cursor-default  uppercase text-lg font-medium  -50 z-10 pointer-events-auto opacity-0'
+        className='h-12 px-2 flex items-center justify-center cursor-default  uppercase text-lg font-medium  z-10 pointer-events-auto opacity-0'
         {...props}
       >
         {t('dock.menu')}
