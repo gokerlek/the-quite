5805a67ccd0fb9de811b4293c9a40ef6188f9264
Göker ZAFER <gokerlek@gmail.com>
2025-10-29 23:01:07 +0300

    Add LeftHouseDoor and RightHouseDoor components with SVG structure for improved visual consistency

---
+++ frontend/src/components/society-events/leftHouseDoor.tsx
@@ -0,0 +1,90 @@
+export const LeftHouseDoor = () => {
+  return (
+    <g id='left-house-door'>
+      <path
+        d='M623 465.5V635H724V465.5V281C668.305 281 623 323.041 623 374.723V465.5ZM623 465.5H651.721'
+        stroke='#1C1C1C'
+        stroke-width='2.56'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+
+      <path
+        d='M723.4 395C684.03 395 652 427.03 652 466.4C652 505.77 684.03 537.8 723.4 537.8'
+        stroke='#F0002C'
+        stroke-width='2'
+        stroke-miterlimit='10'
+      />
+
+      <path d='M723.4 537.8V395' stroke='#F0002C' stroke-width='2' stroke-miterlimit='10' />
+
+      <path
+        d='M723.399 400.7C687.114 400.7 657.699 430.115 657.699 466.4C657.699 502.685 687.114 532.1 723.399 532.1'
+        stroke='#F0002C'
+        stroke-width='2'
+        stroke-miterlimit='10'
+      />
+
+      <path
+        d='M723.399 483.95C716.349 488.03 708.16 490.36 699.43 490.36C681.69 490.36 666.199 480.73 657.909 466.4C657.839 466.27 657.759 466.14 657.689 466.01'
+        stroke='#F0002C'
+        stroke-width='2'
+        stroke-miterlimit='10'
+      />
+
+      <path
+        d='M723.4 400.91L723.02 400.69'
+        stroke='#F0002C'
+        stroke-width='2'
+        stroke-miterlimit='10'
+      />
+
+      <path
+        d='M723.4 483.96C716.12 479.75 710.05 473.69 705.84 466.41C701.76 459.36 699.43 451.17 699.43 442.44C699.43 424.7 709.07 409.21 723.4 400.92C723.52 400.85 723.65 400.77 723.78 400.7'
+        stroke='#F0002C'
+        stroke-width='2'
+        stroke-miterlimit='10'
+      />
+
+      <path
+        d='M723.4 531.88C723.27 531.95 723.14 532.03 723.01 532.1'
+        stroke='#F0002C'
+        stroke-width='2'
+        stroke-miterlimit='10'
+      />
+
+      <path
+        d='M657.689 466.8C657.759 466.67 657.839 466.54 657.909 466.41C666.199 452.08 681.69 442.44 699.43 442.44C708.16 442.44 716.349 444.77 723.399 448.85'
+        stroke='#F0002C'
+        stroke-width='2'
+        stroke-miterlimit='10'
+      />
+
+      <path
+        d='M723.79 532.09C723.66 532.02 723.53 531.95 723.4 531.87C709.07 523.58 699.43 508.09 699.43 490.35C699.43 481.62 701.76 473.44 705.84 466.39C710.05 459.11 716.12 453.05 723.4 448.84'
+        stroke='#F0002C'
+        stroke-width='2'
+        stroke-miterlimit='10'
+      />
+
+      <path
+        d='M676.949 512.85L723.399 466.4'
+        stroke='#F0002C'
+        stroke-width='2'
+        stroke-miterlimit='10'
+      />
+
+      <path
+        d='M723.399 466.4L676.949 419.95'
+        stroke='#F0002C'
+        stroke-width='2'
+        stroke-miterlimit='10'
+      />
+
+      <path
+        d='M831 635H724.001V465.5V458V281C783.003 281 831 323.041 831 374.723V465.5V635Z'
+        fill='#EDEDED'
+      />
+    </g>
+  )
+}
