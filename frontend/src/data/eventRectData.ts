1428dad9f60ada335cbcc760ec1d7f57171362f4
Göker ZAFER <gokerlek@gmail.com>
2025-11-18 15:03:09 +0300

    Move rectData mapping to a separate file for better organization and reuse

---
+++ frontend/src/data/eventRectData.ts
@@ -0,0 +1,9 @@
+export const rectData = {
+  'btn-1':
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sodales ex non urna porttitor fringilla. Quisque eu purus sapien. Cras ultricies ornare leo. Etiam quis convallis lectus. Pellentesque eleifend, elit in fermentum molestie, augue tortor eleifend purus, non dictum lacus dui vel libero. Suspendisse laoreet sem sed nisl laoreet facilisis. Morbi tristique nisi id vehicula elementum. Proin venenatis dolor id metus volutpat pulvinar. Vestibulum varius nisl id sapien sodales porttitor. Phasellus ornare neque eu ligula efficitur vulputate. Aliquam vehicula augue egestas ante ultrices, et sodales tortor ultrices. Duis non lacus a eros elementum scelerisque. Integer quis arcu ipsum. Integer eu aliquam leo. Fusce in cursus mauris.',
+  'btn-2':
+    'Nulla aliquet auctor lacus, eget rhoncus augue ornare sit amet. In sagittis blandit est ut maximus. In hac habitasse platea dictumst. Fusce dapibus arcu at dui ultrices fringilla. ',
+  'btn-3':
+    'Donec at ipsum non nunc sollicitudin interdum. Sed mattis dictum sapien et pretium. Nullam sit amet turpis nunc. Aliquam erat volutpat. Curabitur a metus laoreet nunc dapibus bibendum non vel magna. Suspendisse potenti. Vestibulum ut luctus est, eu aliquet diam. Aenean magna arcu, malesuada ac massa eget, rhoncus viverra sapien. Vestibulum interdum accumsan erat, non tincidunt nunc consequat in. Vivamus lobortis enim justo, lobortis suscipit quam pretium nec.',
+  'btn-4': 'Integer vel purus gravida, mollis dolor idacinia odio nulla ut lectus.',
+}
